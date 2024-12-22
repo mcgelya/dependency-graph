457096aa497c880bccd8d2b451defcc6ae62cb4d
Angelina Zhirnova <angelinazhirnova1407@gmail.com>
2024-12-22 23:31:15 +0300

    Initial commit

---
+++ app/dep_graph.py
@@ -0,0 +1,45 @@
+import networkx as nx
+import asyncio
+import os
+
+graphs = dict()
+
+async def generate_graph(file: str, token: str):
+    global graphs
+
+    proc = await asyncio.create_subprocess_shell(
+        f'pip-compile {file} -o {file}.out',
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    stdout, stderr = await proc.communicate()
+    # if os.path.exists(f'{file}.out'):
+    with open(f'{file}.out', "r") as out:
+        G = nx.DiGraph()
+        root = 'Your project'
+        G.add_node(root)
+        package = None
+        for line in out.readlines():
+            line = line.strip()
+            if len(line) == 0 or line[0] == '#' and not package:
+                continue
+            if line[0] != '#':
+                package, version = line.split('==')
+                G.add_node(package, version=f'=={version}')
+            else:
+                line = line.lstrip(' ')
+                if line.startswith('# via'):
+                    line = line[len('# via'):]
+                line = line.lstrip('# ')
+                if len(line) == 0:
+                    continue
+                if line[0] == '-': # root package
+                    G.add_edge(root, package)
+                else:
+                    G.add_edge(line, package)
+        graphs[token] = (G, None)
+        print('Graph was written correctly')
+    # else:
+    #     graphs[token] = (None, stderr)
+    os.remove(f'{file}')
+    os.remove(f'{file}.out')
---
+++ app/main.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI, Request, File, UploadFile, BackgroundTasks
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pyvis.network import Network
+import networkx as nx
+import os
+
+import dep_graph
+import uuid
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+UPLOAD_DIR = "uploaded_files"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+async def generate_graph_html(G) -> str:
+    net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="#000000")
+    net.from_nx(G)
+    net.set_options("""
+       var options = {
+         "nodes": {
+           "color": {
+             "highlight": {
+               "background": "#FF5733",
+               "border": "#FF5733"
+             }
+           }
+         },
+         "edges": {
+           "color": {
+             "highlight": "#FF5733"
+           }
+         }
+       }
+       """)
+
+    return net.generate_html()
+
+@app.get("/", response_class=HTMLResponse)
+async def main(request: Request, message: str = None):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "title": "Dependency Visualizer", "message": message}
+    )
+
+
+@app.get("/graph/{token}")
+async def show_graph(request: Request, token: str):
+    if token in dep_graph.graphs:
+        G, stderr = dep_graph.graphs[token]
+        if G is not None:
+            graph_html = await generate_graph_html(G)
+            return templates.TemplateResponse(
+                "index.html",
+                {"request": request, "graph_html": graph_html}
+            )
+        else:
+            return templates.TemplateResponse(
+                "index.html",
+                {"request": request, "title": "Dependency Visualizer", "message": f'{stderr}'}
+            )
+    else:
+        return templates.TemplateResponse(
+            "index.html",
+            {"request": request, "title": "Dependency Visualizer",
+             "message": "Graph is being generated. Please keep refreshing page..."}
+        )
+
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile, background_tasks: BackgroundTasks):
+    token = uuid.uuid4().hex
+    file_path = os.path.join(UPLOAD_DIR, token)
+
+    try:
+        with open(file_path, "wb") as buffer:
+            buffer.write(await file.read())
+
+        background_tasks.add_task(dep_graph.generate_graph, file_path, token)
+
+        return RedirectResponse(url=f'/graph/{token}', status_code=303)
+    except Exception as e:
+        return RedirectResponse(url=f'/', status_code=303)
